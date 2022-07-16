08965134ab96cb9c90d59fa11866777839497eeb
johnp <johnpaulnwaz@gmail.com>
2022-07-16 22:39:11 +0100

    made changes to the bash scripts to make them executable and added relevant comments for reference

--- Bash-Scripts/create-azure-ad-aks-group.sh
+++ Bash-Scripts/create-azure-ad-aks-group.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-AZURE_AD_GROUP_NAME="devopsthehardway-aks-group"
-CURRENT_USER_OBJECTID=$(az ad signed-in-user show --query objectId -o tsv)
+# This script creates an az ad group and adds the current signed in user to the ad group
+AZURE_AD_GROUP_NAME="UberApi-aks--ad-group"
+CURRENT_USER_OBJECTID=$(az ad signed-in-user show --query id -o tsv)
 
 # Create Azure AD Group
 az ad group create --display-name $AZURE_AD_GROUP_NAME --mail-nickname $AZURE_AD_GROUP_NAME
@@ -9,6 +10,6 @@ az ad group create --display-name $AZURE_AD_GROUP_NAME --mail-nickname $AZURE_AD
 # Add Current az login user to Azure AD Group
 az ad group member add --group $AZURE_AD_GROUP_NAME --member-id $CURRENT_USER_OBJECTID
 
-AZURE_GROUP_ID=$(az ad group show --group "devopsthehardway-aks-group" --query objectId -o tsv)
+AZURE_GROUP_ID=$(az ad group show --group "UberApi-aks--ad-group" --query id -o tsv)
 
 echo "AZURE AD GROUP ID IS: $AZURE_GROUP_ID"
--- Bash-Scripts/create-terraform-remote-storage.sh
+++ Bash-Scripts/create-terraform-remote-storage.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-RESOURCE_GROUP_NAME  =  "devopsthehardway-rg"
-STORAGE_ACCOUNT_NAME =  "devopsthehardway"
-LOCATION             =  "uksouth"
+# This script creates the resource group, storage account and blob container for the terraform state file
+RESOURCE_GROUP_NAME="tfstate-rg"
+STORAGE_ACCOUNT_NAME="tfstate$RANDOM" 
+CONTAINER_NAME="tfstate"  
 
 # Create Resource Group
-az group create -l $LOCATION -n $RESOURCE_GROUP_NAME
+az group create --location uksouth   --name $RESOURCE_GROUP_NAME
 
 # Create Storage Account
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l $LOCATION --sku Standard_LRS
+az storage account create --name $STORAGE_ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --kind StorageV2 --location uksouth --sku Standard_LRS --encryption-services blob
 
 # Create Storage Account blob
-az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
+az storage container create  --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
